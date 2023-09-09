a360734e80eb830de350f7ceeeb3d38314bca233
Tizzz-555 <103275021+Tizzz-555@users.noreply.github.com>
2023-09-09 21:00:38 +0200

    future only filter

--- rBot/rBot.py
+++ rBot/rBot.py
@@ -139,7 +139,9 @@ reddit = praw.Reddit(
 subreddit = reddit.subreddit("DestinySherpa")
 print("=====================================")
 
-for submission in subreddit.hot(limit=50):
+current_datetime = datetime.now()
+
+for submission in subreddit.hot(limit=20):
     if "[lts]" in submission.title.lower() and "[gos]" not in submission.title.lower():
         if (
             contains_weekend_date(submission.title)
@@ -155,24 +157,32 @@ for submission in subreddit.hot(limit=50):
                 submission.selftext, submission.created_utc
             )
 
-            all_converted_times = [
-                time for _, time in converted_times_title + converted_times_selftext
+            # Filter out past times
+            future_times_title = [
+                (day, t)
+                for day, t in converted_times_title
+                if datetime.strptime(
+                    f"{current_datetime.year}/{day} {t}", "%Y/%A %H:%M"
+                )
+                > current_datetime
+            ]
+            future_times_selftext = [
+                (day, t)
+                for day, t in converted_times_selftext
+                if datetime.strptime(
+                    f"{current_datetime.year}/{day} {t}", "%Y/%A %H:%M"
+                )
+                > current_datetime
             ]
 
-            if all_converted_times:
-                print(
-                    f"{submission.title} (Time: {', '.join(all_converted_times)} CEST)"
+            # Check if the detected times fall within the specified schedule and are in the future
+            if any(
+                is_within_schedule(
+                    day.capitalize(), datetime.strptime(t, "%H:%M").time()
                 )
-            else:
+                for day, t in future_times_title + future_times_selftext
+            ):
                 print(submission.title)
-
-            # Check if the detected times fall within the specified schedule
-            # if any(
-            #     is_within_schedule(
-            #         day.capitalize(), datetime.strptime(t, "%H:%M").time()
-            #     )
-            #     for day, t in converted_times_title + converted_times_selftext
-            # ):
-            #     submission.reply(
-            #         "Hello, I would love to be joining this! I am a Warlock. Discord: tix555#1664 Bungie id: tix555#7313"
-            #     )
+                submission.reply(
+                    "Hello, I would love to be joining this! I am a Warlock. Discord: tix555#1664 Bungie id: tix555#7313"
+                )
